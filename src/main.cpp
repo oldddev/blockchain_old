e18df5c21c559675a4e2e80b822715e006f804b3
Shaik Ali <shaikali961@gmail.com>
2025-06-05 12:22:33 +0000

    Hell yeah, did the halfway through

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include "../include/transaction.h"
-#include <iostream>
+#include "../include/blockchain.h"
 
 using namespace std;
 
 int main(){
-    const Transaction tx("SHAIKALI", "MARWAAN", 1000);
-    cout << tx.get_tx() << endl;
+    Blockchain blockchain;
+
+    const string sender = blockchain.create_user();
+    const string reciever = blockchain.create_user();
+    const string reciever1 = blockchain.create_user();
+    const string reciever2 = blockchain.create_user();
+
+
+    Transaction tx(sender, reciever, 20000);
+    Transaction tx1(sender, reciever1, 121);
+    Transaction tx2(sender, reciever2, 212);
+
+
+
+    blockchain.add_tx(tx);
+    blockchain.add_tx(tx1);
+    blockchain.add_tx(tx2);
+
+    blockchain.show_utxo();
+
+    blockchain.find_unspent(tx2.get_receiver());
+    
     return 0;
 }
